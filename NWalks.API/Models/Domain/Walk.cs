469808e9cb1d952361e8a7260152c6543a65ab70
Sunday-Binakin <sundaybinakin@gmail.com>
2024-05-15 07:18:39 +0000

    first commit

---
+++ NWalks.API/Models/Domain/Walk.cs
@@ -0,0 +1,17 @@
+namespace NWalks.API.Models.Domain;
+
+public class Walk
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public double Lengthinkm { get; set; }
+    public string? walkimageUrl { get; set; }
+
+    public Guid DifficultyId { get; set; }
+
+    public Guid RegionId { get; set; }
+    //Navigation properties
+    public Difficulty Difficulty { get; set; }
+    public Region Region { get; set; }
+}
